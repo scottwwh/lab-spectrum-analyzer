9cb70d3547f9e37c4d12f3f0f67cf3a23805fcd5
Scott Harley <github@scottwwh.ca>
2017-10-19 20:10:18 -0400

    Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,32 @@
-var gulp = require('gulp');
+const gulp        = require('gulp');
+const rollup      = require('rollup');
+const browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
-  // place code for your default task here
+// Rollup - INCOMPLETE
+gulp.task('rollup', async function () {
+    const bundle = await rollup.rollup({
+      input: './src/index.js',
+      plugins: []
+    });
+
+    const isRelease = false;
+    await bundle.write({
+      file: './build/bundle.js',
+      format: 'iife',
+      name: 'SpectrumAnalyzer',
+      sourcemap: !isRelease
+    });
+});
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
+});
+
+gulp.task('default', ['browser-sync'], function() {
+  console.log('HELLO!');
 });
